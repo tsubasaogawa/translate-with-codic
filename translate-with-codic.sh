b3ce2eee966169c027032390c3ee2329121f0726
Tsubasa Ogawa <hurikake@gmail.com>
2017-12-22 11:22:24 +0900

    first commit

---
+++ translate-with-codic.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# usage: translate_with_codic.bash 英訳したい日本語
+
+readonly ACCESS_TOKEN=''
+readonly TEXT=$1
+[[ -z $TEXT ]] && exit 1
+
+RESPONSE=$(curl --silent \
+     -H 'Host: api.codic.jp' \
+     -H "Authorization: Bearer $ACCESS_TOKEN" \
+     "https://api.codic.jp/v1/engine/translate.json?text=$TEXT")
+RETURN_CODE=$?
+[[ $RETURN_CODE -ne 0 ]] && exit $RETURN_CODE
+
+# API の成功可否と翻訳結果だけ得られればよい
+SUCCESSFUL=$(echo $RESPONSE | perl -ne 'print $1 if /"successful":(\w+),/')
+TRANSLATED_TEXT=$(echo $RESPONSE | perl -ne 'print $1 if /"translated_text":"(\w+)"/')
+
+[[ $SUCCESSFUL != 'true' ]] && echo "error. SUCCESSFUL=$SUCCESSFUL" && exit 1
+echo $TRANSLATED_TEXT
+exit 0
